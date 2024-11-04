19860841e1cab96bf2f762fb2c88adc4571f93b3
Eric McGregor <erimcg@users.noreply.github.com>
2024-11-04 03:51:55 +0000

    added flags.s

---
+++ flags.s
@@ -0,0 +1,107 @@
+	.file	"jump.c"
+.LC1:
+	.string	"cmp: %hhd, %hhd \t(CF:%hd ZF:%hd SF:%hd OF:%hd)\n"
+	.text
+	.globl	foo
+	.type	foo, @function
+foo:
+.LFB0:
+	.cfi_startproc
+	movb	%dil, %al
+	movb	%sil, %dl
+	cmpb	%al, %dl
+	pushf
+	movq	$0, %rbx
+	popq	%rbx
+
+    movzbw    %al, %si
+    movzbw    %dl, %dx
+
+	movw	$0x1, %cx
+	andw	%bx, %cx
+	
+	movw	$0x40, %r8w
+	andw	%bx, %r8w
+	shrw	$6, %r8w
+
+	movw	$0x80, %r9w
+	andw	%bx, %r9w
+	shrw	$7, %r9w
+
+	movq	$0x800, %rbp
+	andw	%bx, %bp
+	shrq	$11, %rbp
+    pushq   %rbp
+
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+
+    popq    %rbp
+	movl	$0, %eax
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	foo, .-foo
+	.section	.rodata
+.LC0:
+	.string	"hello %d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	movb 	$1, %dil
+	movb	$1, %sil
+	movl	$0, %eax
+	call	foo
+
+	movb 	$1, %dil
+	movb	$2, %sil
+	movl	$0, %eax
+	call	foo
+
+	movb 	$-1, %dil
+	movb	$127, %sil
+	movl	$0, %eax
+	call	foo
+
+	movb 	$-2, %dil
+	movb	$-1, %sil
+	movl	$0, %eax
+	call	foo
+
+	movb 	$-128, %dil
+	movb	$1, %sil
+	movl	$0, %eax
+	call	foo
+
+	movb 	$1, %dil
+	movb	$-1, %sil
+	movl	$0, %eax
+	call	foo
+	
+	movl	$0, %eax
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
--- hello.s
+++ hello.s
@@ -2,13 +2,17 @@
 	.text
 	.section	.rodata
 .LC0:
-	.string	"9 \\ 2 = %ld r %ld\n"
+	.string	"9 / 2 = %ld r %ld\n"
 .LC1:
-	.string	"7 \\ 2 = %d r %d\n"
+	.string	"-7 / 2 = %d r %d\n"
 .LC2:
-	.string	"5 \\ 2 = %hd r %hd\n"
+	.string	"5 / 2 = %hd r %hd\n"
 .LC3:
-	.string	"3 \\ 2 = %hhd r %hhd\n"
+	.string	"-3 / 2 = %hhd r %hhd\n"
+.LC4:
+	.string	"9 * 2 = %d\n"
+.LC5:
+	.string	"-7 * 2 = %d\n"
 	.text
 	.globl	main
 	.type	main, @function
@@ -16,42 +20,59 @@ main:
 .LFB0:
 	.cfi_startproc
 	movq	$9, %rax
-	movq	$2, %rsi
+	movq	$2, %rcx
 	cqto
-	idivq	%rsi
+	idivq	%rcx
 	movq	%rax, %rsi
 	leaq	.LC0(%rip), %rdi
 	movl	$0, %eax
 	call	printf@PLT
 
-	movl	$7, %eax
-	movl	$2, %esi
+	movl	$-7, %eax
+	movl	$2, %ecx
 	cdq
-	idivl	%esi
+	idivl	%ecx
 	movl	%eax, %esi
 	leaq	.LC1(%rip), %rdi
 	movl	$0, %eax
 	call	printf@PLT
 
 	movw	$5, %ax
-	movw	$2, %si
+	movw	$2, %cx
 	cwd
-	idivw	%si
+	idivw	%cx
 	movw	%ax, %si
 	leaq	.LC2(%rip), %rdi
 	movl	$0, %eax
 	call	printf@PLT
-	movl	$0, %eax
 
-	movw	$3, %ax
-	movb	$2, %sil
-	idivb	%sil
+	movw	$-3, %ax
+	movb	$2, %dl
+	idivb	%dl
 	movb	%al, %sil
-	sarw	$8, %ax
-	movb	%al, %dl
+	movb	%ah, %dl
 	leaq	.LC3(%rip), %rdi
 	movl	$0, %eax
 	call	printf@PLT
+	
+	movl	$9, %edx
+	movl	$2, %esi
+	imull	%edx, %esi
+	leaq	.LC4(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+
+	movw	$-7, %ax
+	movzwl	%ax, %eax
+	movw	$2, %si
+	imulw	%si
+	shll	$16, %edx
+	orl		%edx, %eax
+	movl	%eax, %esi
+	leaq	.LC5(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+
 	movl	$0, %eax
 	ret
 	.cfi_endproc
